0cdaa58c2151597c6c14a465a3482e779c9a1cec
Pablo <2001.paok@gmail.com>
2021-11-02 14:40:00 -0600

    Finished implementation of EigenFaces for facial detection.

--- csv_builder.py
+++ csv_builder.py
@@ -6,10 +6,11 @@ csv_file= open("data.csv", "w")
 img_directory= "/home/paok/Documents/FaceRecognition/trainImages"
 #create a list with file names in image directory
 pictures = listdir(img_directory)
-#Iterate through file anme list and add to csv file
+#Iterate through file name list and add to csv file
+
 for pic in pictures:
     # Format text for CSV file: directory/target/file_name/#number
     # sample: /home/paok/Documents/FaceRecognition/trainImages/target/IMG2.jpg;0
-    out_put = img_directory+"/target/"+pic+";"+str(0)
+    out_put = img_directory+"/"+pic+";"+str(1)
     csv_file.write(out_put)
     csv_file.write("\n")
--- faceRecognizer.cpp
+++ faceRecognizer.cpp
@@ -1,12 +1,19 @@
-#include "opencv2/objdetect.hpp"
+#include <bits/stdc++.h>
+
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
-#include <bits/stdc++.h>
-#include <sys/timeb.h>
-using namespace std;
-using namespace cv;
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace cv::face;
+using namespace std;
+
+Scalar red = Scalar(0,0,255);
 //trainerfromCSV
 //input: src (string with direction of CSV file), labels, images (vectors passed by reference)
 // A function that reads the paths stored in a CSV file and stores said images in a Mat vector.
@@ -17,7 +24,7 @@ void trainerfromCSV(string src, vector<int>&labels, vector<Mat>&images){
     }else{
         string line, path, label;
         while (getline(CSV,line)){
-            stringstream liness;
+            stringstream liness(line);
             //get image path from CSV
             getline(liness,path,';');
             // get ID of face
@@ -31,4 +38,71 @@ void trainerfromCSV(string src, vector<int>&labels, vector<Mat>&images){
             }   
         }
     }
+}
+
+void face_detect(Mat frame, CascadeClassifier target, vector<Rect>& Instances, double scale, Ptr<EigenFaceRecognizer> model, int modelwidth, int modelheigh){
+    Mat grayFrame;
+    cvtColor(frame, grayFrame, COLOR_BGR2GRAY );
+    equalizeHist( grayFrame, grayFrame );
+    resize(grayFrame, grayFrame,Size(grayFrame.size().width /scale,grayFrame.size().height /scale));
+    target.detectMultiScale(grayFrame, Instances,1.1,3,0,Size(25,25));
+    
+
+    for (int i = 0; i < Instances.size(); i++){
+        Rect realArea = Rect(cvRound(Instances[i].x*scale),cvRound(Instances[i].y*scale),cvRound(Instances[i].width*scale),cvRound(Instances[i].height*scale));
+        //crop the face
+        Mat face = grayFrame(Instances[i]);
+        //Resize the image to training images size (necessaty when using Eigen or Fisher faces)
+        Mat predict;
+        resize(face, predict, Size(modelwidth, modelheigh), 1.0, 1.0, INTER_CUBIC);
+        int id = model->predict(predict);
+        string prediction = format("Detected ID %d", id);
+        putText(frame,prediction,Point(realArea.x -10,realArea.y-20),FONT_HERSHEY_PLAIN, 1.0, Scalar(0,255,0), 4);
+        rectangle(frame,realArea, red,6);
+        imshow("Detector", frame);
+    }
+
+    if (Instances.size()==0){
+        imshow("Detector",frame);
+    }
+}
+int main(){
+    // create the vectors to train model
+    vector<Mat> images;
+    vector<int> labels;
+    string src = "data.csv";
+    trainerfromCSV(src,labels,images);
+    cout<< images.size()<<endl;
+    int train_width = images[0].cols;
+    int train_height = images[0].rows;
+    Ptr<EigenFaceRecognizer> model = EigenFaceRecognizer::create();
+    model->train(images, labels);
+    
+    // Load Haar Cacade data for faces
+    CascadeClassifier faces_haar;
+    faces_haar.load("/home/paok/Documents/FaceRecognition/haarcascade_frontalface_alt2.xml");
+    // Create OpenCV frame object to store frame information
+    Mat frame;
+    // Create VideoCapture object, reading video device (USB camera)
+    VideoCapture camera(0);
+    vector<Rect>facesID;
+    // Check if the camera is readable
+    if(!camera.isOpened()){
+        cout<<"Error reading camera feed";
+        return -1;
+    }
+    for (;;){
+        camera.read(frame);
+        // Check if selected device is sending information
+        if(frame.empty()){
+            cout<<"NULL frame ";
+            break;
+        }
+        face_detect(frame,faces_haar,facesID,3.8,model,train_width,train_height);
+        // Read key board input, setting esc as break key
+        if(waitKey(5)== 27){
+            break;
+        }
+    }
+    return 0;
 }
